bc5cef66972cc2915e83c74996697dd8714c4b27
shreya <shreyareddykethireddy@shreyas-MacBook-Air-2.local>
2020-11-25 02:30:55 +0530

    [Shreya]Added: remove emp payroll details

--- js/home.js
+++ js/home.js
@@ -26,8 +26,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
     <td>${empPayrollData._salary}</td>
     <td>${stringifyDate(empPayrollData._startDate)}</td>
     <td>
-        <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
-        <img name="${empPayrollData._id}" alt="edit" onclick="update(this)" src="../assets/create-black-18dp.svg">
+        <img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/delete-black-18dp.svg" alt="delete">
+        <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/create-black-18dp.svg" alt="edit">
     </td>
  </tr>
     `;
@@ -42,3 +42,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
      }
      return deptHtml;
  };
+
+ const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node._id);
+    if(!empPayrollData) return;
+    const index = empPayrollList.map(empData => empData._id)
+                                .indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+};
