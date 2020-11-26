dfd3cebb5dc4a0df108006db9152e80563ac7e9b
shreya <shreyareddykethireddy@shreyas-MacBook-Air-2.local>
2020-11-26 23:16:07 +0530

    [shreya]Modified: local storage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -24,9 +24,21 @@ window.addEventListener('DOMContentLoaded',(event) => {
 const save = () => {
     try{
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     }catch(e){
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll = () =>{
